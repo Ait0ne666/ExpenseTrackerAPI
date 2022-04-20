3d14f3b85dda90caa4259bc0f23ce58c3135fabe
Ait0ne666 <yukhanov@lilekov-studio.com>
2022-04-20 16:33:40 +0700

    added gotm model fields to models

--- models/expenses.go
+++ models/expenses.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Currency string
 
@@ -12,20 +16,26 @@ const (
 )
 
 type Category struct {
-	ID     string `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Title  string `json:"title" gorm:"title;"`
-	UserID string `json:"user_id" gorm:"user_id;default:be9e19d1-7150-4a6d-85f0-1cee1c3ac7bc"`
+	ID        string         `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CreatedAt time.Time      `json:"created_at" gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at" swaggertype:"string" format:"date-time"`
+	Title     string         `json:"title" gorm:"title;"`
+	UserID    string         `json:"user_id" gorm:"user_id;default:be9e19d1-7150-4a6d-85f0-1cee1c3ac7bc"`
 }
 
 type Expense struct {
-	ID         string    `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	Title      *string   `json:"title" gorm:"title"`
-	Date       time.Time `json:"date" gorm:"date"`
-	Category   Category  `json:"category"`
-	Amount     float64   `json:"amount" gorm:"amount"`
-	CategoryID string    `json:"category_id" gorm:"category_id"`
-	Currency   Currency  `json:"currency" gorm:"currency;default:tbh"`
-	UserID     string    `json:"user_id" gorm:"user_id;default:be9e19d1-7150-4a6d-85f0-1cee1c3ac7bc"`
+	ID         string         `json:"id" gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	CreatedAt  time.Time      `json:"created_at" gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt  time.Time      `json:"updated_at" gorm:"column:updated_at;autoUpdateTime"`
+	DeletedAt  gorm.DeletedAt `json:"deleted_at" gorm:"column:deleted_at" swaggertype:"string" format:"date-time"`
+	Title      *string        `json:"title" gorm:"title"`
+	Date       time.Time      `json:"date" gorm:"date"`
+	Category   Category       `json:"category"`
+	Amount     float64        `json:"amount" gorm:"amount"`
+	CategoryID string         `json:"category_id" gorm:"category_id"`
+	Currency   Currency       `json:"currency" gorm:"currency;default:tbh"`
+	UserID     string         `json:"user_id" gorm:"user_id;default:be9e19d1-7150-4a6d-85f0-1cee1c3ac7bc"`
 }
 
 type ExpenseDTO struct {
